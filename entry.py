3dce08663d32ce0684d29207f52d31fa31a5285c
João Cunha <skidrow.jc@gmail.com>
2021-12-07 12:11:57 +0000

    Listing and Editing
    Now list all entries and its possible to delete and edit new entries
    
    View: #2

--- entry.py
+++ entry.py
@@ -1,28 +1,32 @@
+from format_error import FormatError
+
 class Entry:
-    def __init__(self, price, code, ntiers, size, obs):
+    def __init__(self, price, code, ntires, size, obs):
         try:
             price_as_float = float(price);
         except:
             raise FormatError("Preço mal formatado use . em vez de ,")
         self.price = (price_as_float * 0.23) + price_as_float
+        self.price = str(self.price)
 
         try:
-            int_as_code = int(price);
+            int_as_code = int(code);
         except:
-            raise FormatError("Codigo deve ser um numero")
+            raise FormatError("Código deve ser um numero")
         self.code = code
 
         try:
-            int_as_ntiers = int(ntiers);
+            int_as_ntires = int(ntires);
         except:
             raise FormatError("Numero de Pneus deve ser um numero")
-        self.ntiers = int_as_ntiers
+        self.ntires = int_as_ntires
+        self.ntires = str(self.ntires)
 
         self.size = size
         self.obs = obs
 
     def __str__(self):
-        return f" {self.price}€, {self.code}"
+        return f"Preço: {self.price}€, Código: {self.code}, Numero Pneus: {self.ntires} Obs: {self.obs}"
 
     def __repr__(self):
             return str(self)
