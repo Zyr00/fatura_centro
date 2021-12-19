1e2a2914d15272c44ff8eb3119398085e711f53a
João Cunha <skidrow.jc@gmail.com>
2021-12-19 00:16:36 +0000

    Fix smaall bug on files
    the try catch should catch eny error in the function
    
    View: #5

--- files.py
+++ files.py
@@ -19,24 +19,25 @@ class File:
     @staticmethod
     def save(filename, bills, codes):
         bill_formated = []
-        for i in bills:
-            for j in i.entries:
-                group_and_des = File.get_from_code(j.code, codes)
-                bill_formated.append([
-                    i.registration,
-                    i.date,
-                    j.code,
-                    group_and_des[0],
-                    group_and_des[1],
-                    i.kms,
-                    j.ntires,
-                    j.size,
-                    i.supplier,
-                    j.obs,
-                    "Aces Alto Ave Guimarães"
-                    ])
 
         try:
+            for i in bills:
+                for j in i.entries:
+                    group_and_des = File.get_from_code(j.code, codes)
+                    bill_formated.append([
+                        i.registration,
+                        i.date,
+                        j.code,
+                        group_and_des[0],
+                        group_and_des[1],
+                        i.kms,
+                        j.ntires,
+                        j.size,
+                        i.supplier,
+                        j.obs,
+                        "Aces Alto Ave Guimarães"
+                        ])
+
             with open(filename, 'w') as file:
                 writer = csv.writer(file)
                 writer.writerow(File.fields)
